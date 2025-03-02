3e17a53919f5bab15a8e4587e380b2dc295f9082
Alwin Arrasyid <alwin.ridd@gmail.com>
2025-03-03 03:31:30 +0700

    Refactor serial port handling into a dedicated SerialPort struct with initialization, configuration, and reset methods

--- src/main.zig
+++ src/main.zig
@@ -1,53 +1,130 @@
 const std = @import("std");
 const native_os = @import("builtin").os.tag;
 
-fn configure_serial_port(fd: std.posix.fd_t, baud: std.posix.speed_t) !void {
-    var tty = try std.posix.tcgetattr(fd);
-    tty.ispeed = baud;
-    tty.ospeed = baud;
+const SerialPort = struct {
+    path: []const u8,
+    baud: std.posix.speed_t,
+    fd: ?std.posix.fd_t,
+    tty: ?std.posix.termios,
 
-    tty.cflag = .{
-        .CSIZE = .CS8,
-        .CREAD = true,
-        .CLOCAL = true,
-        .PARENB = false,
-        .CSTOPB = false,
-    };
-
-    if (native_os == .macos) {
-        tty.cflag.CCTS_OFLOW = false;
-        tty.cflag.CRTS_IFLOW = false;
+    pub fn init(path: []const u8, baud: std.posix.speed_t) !SerialPort {
+        var port = SerialPort{
+            .path = path,
+            .baud = baud,
+            .fd = null,
+            .tty = null,
+        };
+        try port.open(path);
+        try port.configure();
+        return port;
     }
 
-    tty.lflag = .{
-        .ICANON = false,
-        .ECHO = false,
-        .ECHOE = false,
-        .ECHONL = false,
-        .ISIG = false,
+    pub fn open(self: *SerialPort, path: []const u8) !void {
+        const flag: std.posix.O = .{
+            .ACCMODE = .RDWR,
+            .NONBLOCK = true,
+            .NOCTTY = true,
+        };
+
+        self.fd = try std.posix.open(path, flag, 0);
+    }
+
+    pub fn reset(self: *SerialPort) !void {
+        const fd = self.fd orelse {
+            return error.InvalidFileDescriptor;
+        };
+        switch (native_os) {
+            .macos => {
+                std.posix.close(fd);
+
+                std.time.sleep(100_000_000);
+                try self.open(self.path);
+            },
+            else => unreachable,
+        }
+    }
+
+    fn configure(self: *SerialPort) !void {
+        const fd = self.fd orelse @panic("Invalid file descriptor");
+        var tty = try std.posix.tcgetattr(fd);
+        self.tty = tty;
+        tty.ispeed = self.baud;
+        tty.ospeed = self.baud;
+
+        tty.cflag = .{
+            .CSIZE = .CS8,
+            .CREAD = true,
+            .CLOCAL = true,
+            .PARENB = false,
+            .CSTOPB = false,
+            .HUPCL = true,
+        };
+
+        if (native_os == .macos) {
+            tty.cflag.CCTS_OFLOW = false;
+            tty.cflag.CRTS_IFLOW = false;
+        }
+
+        tty.lflag = .{
+            .ICANON = false,
+            .ECHO = false,
+            .ECHOE = false,
+            .ECHONL = false,
+            .ISIG = false,
+        };
+
+        tty.iflag = .{
+            .IXOFF = false,
+            .IXON = false,
+            .IXANY = false,
+            .IGNBRK = false,
+            .BRKINT = false,
+            .PARMRK = false,
+            .INLCR = false,
+            .IGNCR = false,
+            .ICRNL = false,
+        };
+
+        tty.oflag = .{
+            .OPOST = false,
+            .ONLCR = false,
+        };
+
+        tty.cc[@as(usize, @intFromEnum(std.posix.V.MIN))] = 0;
+        tty.cc[@as(usize, @intFromEnum(std.posix.V.TIME))] = 10;
+
+        try std.posix.tcsetattr(fd, .NOW, tty);
+    }
+
+    pub fn deinit(self: *SerialPort) void {
+        if (self.fd) |fd| {
+            _ = std.posix.close(fd);
+        }
+    }
+};
+
+fn open_serial_port(port_name: []const u8) !std.posix.fd_t {
+    const flag: std.posix.O = .{
+        .ACCMODE = .RDWR,
+        .NONBLOCK = true,
+        .NOCTTY = true,
     };
+    return std.posix.open(port_name, flag, 0);
+}
 
-    tty.iflag = .{
-        .IXOFF = false,
-        .IXON = false,
-        .IXANY = false,
-        .IGNBRK = false,
-        .BRKINT = false,
-        .PARMRK = false,
-        .INLCR = false,
-        .IGNCR = false,
-        .ICRNL = false,
-    };
+const Mode = enum {
+    Reset,
+    Read,
+};
 
-    tty.oflag = .{
-        .OPOST = false,
-        .ONLCR = false,
-    };
-
-    tty.cc[@as(usize, @intFromEnum(std.posix.V.MIN))] = 0;
-    tty.cc[@as(usize, @intFromEnum(std.posix.V.TIME))] = 10;
-
-    try std.posix.tcsetattr(fd, .NOW, tty);
+pub fn get_mode_from(mode: []const u8) !Mode {
+    if (std.mem.eql(u8, mode, "reset")) {
+        return .Reset;
+    } else if (std.mem.eql(u8, mode, "read")) {
+        return .Read;
+    } else {
+        return error.InvalidModeProvided;
+    }
 }
 
 pub fn main() !void {
@@ -56,18 +133,26 @@ pub fn main() !void {
     defer arena.deinit();
 
     var args_iter = try std.process.argsWithAllocator(arena.allocator());
-    if (args_iter.skip() == false) {
-        std.log.err("No arguments provided", .{});
-        return error.NoArgumentsProvided;
+    defer args_iter.deinit();
+    _ = args_iter.skip();
+
+    if (args_iter.inner.count != 4) {
+        usage();
+        return error.InvalidArgumentsProvided;
     }
 
+    const mode_str = args_iter.next() orelse {
+        usage();
+        return error.NoModeProvided;
+    };
+
     const port_name = args_iter.next() orelse {
-        std.log.err("No serial port is provided", .{});
+        usage();
         return error.NoPortProvided;
     };
 
     const baud_rate = args_iter.next() orelse {
-        std.log.err("No baud rate is provided", .{});
+        usage();
         return error.NoBaudRateProvided;
     };
 
@@ -75,28 +160,35 @@ pub fn main() !void {
     const baud: std.posix.speed_t = @enumFromInt(try std.fmt.parseInt(u64, baud_rate, 10));
     std.log.info("Baud rate: {any}", .{baud});
 
-    const flag: std.posix.O = .{
-        .ACCMODE = .RDWR,
-        .NONBLOCK = true,
-        .NOCTTY = true,
-    };
-    const fd = try std.posix.open(
-        port_name,
-        flag,
-        0,
-    );
-    defer std.posix.close(fd);
+    var port = try SerialPort.init(port_name, baud);
+    defer port.deinit();
 
-    std.log.info("Port opened successfully. FD({})", .{fd});
-    try configure_serial_port(fd, baud);
+    std.log.info("Port opened successfully. FD({any})", .{port.fd});
 
-    var buf = try arena.allocator().alloc(u8, 1024);
-    defer arena.allocator().free(buf);
-    while (true) {
-        const bytes_read = try std.posix.read(fd, buf);
-        if (bytes_read == 0) {
-            continue;
-        }
-        try std.io.getStdOut().writer().writeAll(buf[0..bytes_read]);
+    const mode = try get_mode_from(mode_str);
+    switch (mode) {
+        .Read => {
+            var buf = try arena.allocator().alloc(u8, 1024);
+            defer arena.allocator().free(buf);
+            const fd = port.fd orelse {
+                return error.InvalidFileDescriptor;
+            };
+            while (true) {
+                const bytes_read = try std.posix.read(fd, buf);
+                if (bytes_read == 0) {
+                    continue;
+                }
+                try std.io.getStdOut().writer().writeAll(buf[0..bytes_read]);
+            }
+        },
+
+        .Reset => {
+            try port.reset();
+            std.log.info("Port reset successfully.", .{});
+        },
     }
+}
+
+fn usage() void {
+    std.debug.print("Usage: serial_reader <mode> <port> <baud_rate>\n", .{});
 }
